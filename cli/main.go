ea50669640ca8b79c37553c9df182a1339baf51b
Rim <rim@3yourmind.com>
2021-07-12 22:45:13 +0200

    send requests from cli

--- cli/main.go
+++ cli/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
+	"flag"
+	"log"
+	"net/http"
 
 	"github.com/rimvydaszilinskas/announcer-backend/models"
 	"github.com/rimvydaszilinskas/announcer-backend/network"
@@ -10,16 +13,23 @@ import (
 )
 
 func main() {
+	token := flag.String("token", "", "IOT device token")
+	flag.Parse()
+
+	if len(*token) == 0 {
+		log.Fatalf("No token provided")
+	}
+
 	networkInterface, err := network.GetActiveNetworkInterface()
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("error collecting network interface - %s\n", err)
 	}
 
 	system, err := system.CollectSystem()
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("error collecting system - %s\n", err)
 	}
 
 	full := models.FullSystemDetails{
@@ -27,11 +37,31 @@ func main() {
 		NetworkInterface: *networkInterface,
 	}
 
-	d, err := json.Marshal(full)
+	data, err := json.Marshal(full)
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("error marshalling data - %s\n", err)
 	}
 
-	fmt.Println(string(d))
+	req, err := http.NewRequest("POST", "http://localhost:8080/iot/", bytes.NewBuffer(data))
+
+	if err != nil {
+		log.Fatalf("error creating request - %s\n", err)
+	}
+
+	req.Header.Set("Authorization", *token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalf("error executing request - %s\n", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent {
+		log.Fatalf("retrieved a bad error code - %d", resp.StatusCode)
+	}
+	log.Println("OK")
 }
